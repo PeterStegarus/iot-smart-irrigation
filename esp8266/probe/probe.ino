6bc8fb8bab2379f1da3ae121f4429e5f0024362e
Peter Stegarus <stegarusp@gmail.com>
2023-05-01 11:16:30 +0300

    esp8266: Send secure http get request every 5 seconds

--- esp8266/probe/probe.ino
+++ esp8266/probe/probe.ino
@@ -1,5 +1,14 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+
+#define WIFI_SSID "Steag"
+#define WIFI_PASSWORD ""
+#define URL "https://bamboohut.studio/api/properties"
 #define ONE_SECOND_TICKS 312500 // 312500 ticks * 3.2 us / tick = 1 second
 
+
+WiFiClientSecure client;
+HTTPClient http;
 bool isHello;
 int helloCount;
 
@@ -28,15 +37,46 @@ void setupTimerInterrupt() {
   timer1_write(5 * ONE_SECOND_TICKS);
 }
 
+void setupWifi() {
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print("Connecting..");
+  }
+}
+
+void setupHttpClient() {
+  client.setInsecure();
+}
+
 void setup() {
   setupVariables();
   setupSerial();
   setupTimerInterrupt();
+  setupWifi();
+  setupHttpClient();
+}
+
+String sendHttp() {
+    String payload;
+    http.begin(client, URL);
+    int httpCode = http.GET();
+
+    Serial.println("HTTP Code: " + String(httpCode));
+    if (httpCode > 0) {
+      payload = http.getString();
+    }
+
+    http.end();
+
+    return payload;
 }
 
 void loop() {
   if (isHello) {
     hello();
+    Serial.println(sendHttp());
   }
 }
 
