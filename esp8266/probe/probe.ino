1197ac44ebb7106805d605ef093174995fa76523
Peter Stegarus <stegarusp@gmail.com>
2023-05-01 16:36:34 +0300

    esp8266: Add ap mode for with wifi credentials initialization form

--- esp8266/probe/probe.ino
+++ esp8266/probe/probe.ino
@@ -1,14 +1,24 @@
-#include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266WiFi.h>
+#include <FS.h>
 
-#define WIFI_SSID "Steag"
-#define WIFI_PASSWORD ""
+#define WIFI_AP_SSID "Probe"
+#define WIFI_AP_PASSWORD ""
+
 #define URL "https://bamboohut.studio/api/properties"
-#define ONE_SECOND_TICKS 312500 // 312500 ticks * 3.2 us / tick = 1 second
 
+#define ONE_SECOND_TICKS 312500  // 312500 ticks * 3.2 us / tick = 1 second
+
+ESP8266WebServer server(80);
 
 WiFiClientSecure client;
 HTTPClient http;
+
+int status;  // 0 - AP mode; 1 - connecting to wifi; 2 - connected to wifi
+String wifiSsid;
+String wifiPassword;
+
 bool isHello;
 int helloCount;
 
@@ -17,11 +27,10 @@ void hello() {
   Serial.println("Hello " + String(++helloCount));
 }
 
-void ICACHE_RAM_ATTR onTimerISR(){
-  isHello = true;
-}
+void ICACHE_RAM_ATTR onTimerISR() { isHello = true; }
 
 void setupVariables() {
+  status = 0;
   isHello = false;
   helloCount = 0;
 }
@@ -33,50 +42,92 @@ void setupSerial() {
 
 void setupTimerInterrupt() {
   timer1_attachInterrupt(onTimerISR);
-  timer1_enable(TIM_DIV256, TIM_EDGE, TIM_LOOP); // 1 tick = 3.2us
+  timer1_enable(TIM_DIV256, TIM_EDGE, TIM_LOOP);  // 1 tick = 3.2us
   timer1_write(5 * ONE_SECOND_TICKS);
 }
 
 void setupWifi() {
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  server.handleClient();
+  WiFi.begin(wifiSsid, wifiPassword);
 
   while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.print("Connecting..");
+    server.handleClient();
+    delay(250);
+    Serial.println("Connecting...");
   }
+
+  status = 2;
+  server.close();
+  Serial.println("Connected.");
 }
 
-void setupHttpClient() {
-  client.setInsecure();
+void setupHttpClient() { client.setInsecure(); }
+
+void setupAp() {
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP(WIFI_AP_SSID, WIFI_AP_PASSWORD);
+  Serial.print("IP: ");
+  Serial.println(WiFi.softAPIP());
+}
+
+void handleRoot() {
+  server.sendHeader("Location", String("/index.html"), true);
+  server.send(302, "text/plain", "");
+}
+
+void handleSubmit() {
+  wifiSsid = server.arg("ssid");
+  wifiPassword = server.arg("password");
+  status = 1;
+  server.sendHeader("Location", String("/connecting.html"), true);
+  server.send(302, "text/plain", "");
+}
+
+void setupWebServer() {
+  SPIFFS.begin();
+  server.serveStatic("/index.html", SPIFFS, "/index.html");
+  server.serveStatic("/connecting.html", SPIFFS, "/connecting.html");
+  server.serveStatic("/style.css", SPIFFS, "/style.css");
+  server.on("/", handleRoot);
+  server.on("/submit", handleSubmit);
+  server.begin();
 }
 
 void setup() {
   setupVariables();
   setupSerial();
   setupTimerInterrupt();
-  setupWifi();
+  setupAp();
+  setupWebServer();
   setupHttpClient();
 }
 
 String sendHttp() {
-    String payload;
-    http.begin(client, URL);
-    int httpCode = http.GET();
+  String payload;
+  http.begin(client, URL);
+  int httpCode = http.GET();
 
-    Serial.println("HTTP Code: " + String(httpCode));
-    if (httpCode > 0) {
-      payload = http.getString();
-    }
+  Serial.println("HTTP Code: " + String(httpCode));
+  if (httpCode > 0) {
+    payload = http.getString();
+  }
 
-    http.end();
+  http.end();
 
-    return payload;
+  return payload;
 }
 
 void loop() {
-  if (isHello) {
-    hello();
-    Serial.println(sendHttp());
+  switch (status) {
+    case 0:
+      server.handleClient();
+      break;
+    case 1:
+      setupWifi();
+    default:
+      if (isHello) {
+        hello();
+        Serial.println(sendHttp());
+      }
   }
 }
-
