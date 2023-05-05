40febee9ca3027b971879b2dd49285607f78bdc9
Peter Stegarus <stegarusp@gmail.com>
2023-05-05 19:09:18 +0300

    server: Add node and zone models

---
+++ server/models/node.js
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose';
+
+const nodeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  zone: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+    enum: ['probe', 'actuator'],
+  },
+  status: {
+    type: String,
+    required: true,
+    enum: ['on', 'off'],
+    default: 'off',
+  },
+  history: [{
+    timestamp: {
+      type: Date,
+      required: true,
+    },
+    action: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: String,
+      required: true,
+    },
+  }],
+});
+
+const Node = mongoose.model('Node', nodeSchema);
+
+export default Node;
