ea076333f5a0c8520dc5fbb0a0f07ae040e0e968
Peter Stegarus <stegarusp@gmail.com>
2023-05-05 22:20:34 +0300

    server: Add endpoints for getting and posting humidity

--- server/controllers/nodes.js
+++ server/controllers/nodes.js
@@ -3,6 +3,8 @@ import Node from '../models/node.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
+const ONE_MINUTE = 60 * 1000;
+
 const createToken = (node) => {
   return jwt.sign({ id: node._id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRATION });
 };
@@ -43,5 +45,55 @@ export const register = async (req, res) => {
     return error.code === 11000
       ? res.status(409).json({ message: "Node already exists" })
       : res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const postHumidity = async (req, res) => {
+  const node = req.user;
+  const humidity = req.body.humidity;
+
+  if (!humidity) return res.status(400).json({ message: "Humidity is required" });
+
+  node.history.push({
+    timestamp: new Date().toISOString(),
+    action: 'humidity',
+    value: humidity,
+  });
+  await node.save();
+
+  return res.status(200).json({ message: "Humidity updated" });
+};
+
+export const getHumidity = async (req, res) => {
+  const node = req.user;
+  const zone = node.zone;
+  const currentTime = new Date().getTime();
+
+  try {
+    const nodes = await Node.find({ zone: zone.toLowerCase() });
+    const recentHumidityData = nodes.flatMap((node) =>
+      node.history
+        .filter((historyItem) =>
+          historyItem.action === 'humidity'
+          && currentTime - new Date(historyItem.timestamp).getTime() < ONE_MINUTE
+        ).map((historyItem) =>
+          historyItem.value
+        )
+    );
+
+    const averageHumidity = recentHumidityData.reduce((acc, humidity) => acc + parseInt(humidity), 0) / nodes.length;;
+
+    console.log(averageHumidity);
+
+    node.history.push({
+      timestamp: new Date().toISOString(),
+      action: 'getHumidity',
+      value: averageHumidity,
+    });
+
+    return res.status(200).json({ humidity: averageHumidity });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Server error" });
   }
 };
