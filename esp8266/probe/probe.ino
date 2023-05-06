a592bd38d22094ef7d7f6d47c390d1ce619a7385
Peter Stegarus <stegarusp@gmail.com>
2023-05-06 16:00:07 +0300

    esp8266: Clean up probe

--- esp8266/probe/probe.ino
+++ esp8266/probe/probe.ino
@@ -1,43 +1,35 @@
+#include <ArduinoJson.h>
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266WiFi.h>
 #include <FS.h>
 
+#define ONE_SECOND_TICKS 312500  // 312500 ticks * 3.2 us / tick = 1 second
 #define WIFI_AP_SSID "Probe"
 #define WIFI_AP_PASSWORD ""
 
-#define URL "https://bamboohut.studio/api/properties"
-
-#define ONE_SECOND_TICKS 312500  // 312500 ticks * 3.2 us / tick = 1 second
+const String SWARM_URL = "https://irrigation-system.blospera.live/api";
 
 ESP8266WebServer server(80);
-
 WiFiClientSecure client;
 HTTPClient http;
 
-int status;  // 0 - AP mode; 1 - connecting to wifi; 2 - connected to wifi
 String wifiSsid;
 String wifiPassword;
+String nodeName;
+String nodeZone;
 
-bool isHello;
-int helloCount;
+bool canSendRequest;
+String response;
+String token;
 
-void hello() {
-  isHello = false;
-  Serial.println("Hello " + String(++helloCount));
-}
+void ICACHE_RAM_ATTR onTimerISR() { canSendRequest = true; }
 
-void ICACHE_RAM_ATTR onTimerISR() { isHello = true; }
-
-void setupVariables() {
-  status = 0;
-  isHello = false;
-  helloCount = 0;
-}
+void setupVariables() { canSendRequest = false; }
 
 void setupSerial() {
   Serial.begin(9600);
-  Serial.println("Init");
+  Serial.println("Initialized");
 }
 
 void setupTimerInterrupt() {
@@ -46,51 +38,70 @@ void setupTimerInterrupt() {
   timer1_write(5 * ONE_SECOND_TICKS);
 }
 
-void setupWifi() {
-  server.handleClient();
-  WiFi.begin(wifiSsid, wifiPassword);
+void setupAp() {
+  WiFi.mode(WIFI_AP_STA);
+  WiFi.softAP(WIFI_AP_SSID, WIFI_AP_PASSWORD);
+  Serial.print("AP IP: ");
+  Serial.println(WiFi.softAPIP());
+}
 
+void setupWebServer() {
+  SPIFFS.begin();
+  server.serveStatic("/index.html", SPIFFS, "/index.html");
+  server.serveStatic("/style.css", SPIFFS, "/style.css");
+  server.on("/", handleRoot);
+  server.on("/submit", handleSubmit);
+  server.begin();
+}
+
+void setupWifi() {
+  WiFi.begin(wifiSsid, wifiPassword);
   while (WiFi.status() != WL_CONNECTED) {
-    server.handleClient();
-    delay(250);
+    delay(1000);
     Serial.println("Connecting...");
   }
 
-  status = 2;
-  server.close();
-  Serial.println("Connected.");
+  WiFi.setAutoReconnect(true);
+  WiFi.persistent(true);
+
+  Serial.print("Connected. WiFi IP: ");
+  Serial.println(WiFi.localIP());
 }
 
 void setupHttpClient() { client.setInsecure(); }
-
-void setupAp() {
-  WiFi.mode(WIFI_AP);
-  WiFi.softAP(WIFI_AP_SSID, WIFI_AP_PASSWORD);
-  Serial.print("IP: ");
-  Serial.println(WiFi.softAPIP());
-}
 
 void handleRoot() {
   server.sendHeader("Location", String("/index.html"), true);
   server.send(302, "text/plain", "");
 }
 
+void setTokenFromServer() {
+  String payload;
+  StaticJsonDocument<200> doc;
+  doc["name"] = nodeName;
+  doc["zone"] = nodeZone;
+  serializeJson(doc, payload);
+
+  http.begin(client, SWARM_URL + "/nodes/login");
+  http.addHeader("Content-Type", "application/json");
+  int httpCode = http.POST(payload);
+  payload = http.getString();
+  http.end();
+
+  deserializeJson(doc, payload);
+  token = String(doc["token"]);
+}
+
 void handleSubmit() {
   wifiSsid = server.arg("ssid");
   wifiPassword = server.arg("password");
-  status = 1;
-  server.sendHeader("Location", String("/connecting.html"), true);
-  server.send(302, "text/plain", "");
-}
+  nodeName = server.arg("name");
+  nodeZone = server.arg("zone");
 
-void setupWebServer() {
-  SPIFFS.begin();
-  server.serveStatic("/index.html", SPIFFS, "/index.html");
-  server.serveStatic("/connecting.html", SPIFFS, "/connecting.html");
-  server.serveStatic("/style.css", SPIFFS, "/style.css");
-  server.on("/", handleRoot);
-  server.on("/submit", handleSubmit);
-  server.begin();
+  setupWifi();
+  setTokenFromServer();
+
+  server.send(200, "text/plain", "");
 }
 
 void setup() {
@@ -100,34 +111,32 @@ void setup() {
   setupAp();
   setupWebServer();
   setupHttpClient();
+
+  // Temporary
+  wifiSsid = "Steag";
+  wifiPassword = "";
+  nodeName = "probe-1";
+  nodeZone = "zone-1";
+  setupWifi();
+  setTokenFromServer();
 }
 
-String sendHttp() {
-  String payload;
-  http.begin(client, URL);
+String sendHttpGet() {
+  canSendRequest = false;
+
+  http.begin(client, SWARM_URL + "/nodes/humidity");
+  http.addHeader("Authorization", "Bearer " + token);
   int httpCode = http.GET();
-
-  Serial.println("HTTP Code: " + String(httpCode));
-  if (httpCode > 0) {
-    payload = http.getString();
-  }
-
+  String payload = http.getString();
   http.end();
 
   return payload;
 }
 
 void loop() {
-  switch (status) {
-    case 0:
-      server.handleClient();
-      break;
-    case 1:
-      setupWifi();
-    default:
-      if (isHello) {
-        hello();
-        Serial.println(sendHttp());
-      }
+  server.handleClient();
+  if (canSendRequest && !token.isEmpty()) {
+    response = sendHttpGet();
+    Serial.println(response);
   }
 }
